855033b8b7c8b20f3e21b9d596adb6de61c77d84
duongse141129 <duong97498@gmail.com>
2020-11-16 08:03:55 +0700

    add junit

--- src/duong/util/MathUtil.java
+++ src/duong/util/MathUtil.java
@@ -29,4 +29,13 @@ public class MathUtil {
         //in thủ PI
         System.out.println("PI: "+PI);
     }
+    //muốn kết luận hàm minh họa, chạy bằng mắt
+    //so sánh vs kì vọng
+    //các này ổn mất sức
+    //Nâng cao: dùng màu sắc xanh đỏ kết luận
+    //ta thấy xanh, hàm ý mọi tình huống ổn
+    //ta xài thêm 1 framework, unit test tên chung
+    //junit,testing,nunit.. là những framework cụ thể đẻ giúp test các hàm
+    //1 cách hiệu quả
+    //dùn nó ta tích hợp vào 1 quy trình làm phần mềm
 }
---
+++ test/fu/util/test/MathUtinityTest.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fu.util.test;
+
+import static duong.util.MathUtility.*;
+import java.sql.SQLException;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Admin
+ */
+public class MathUtinityTest {
+    //test những hàm trên
+    //các đặt tên hàm nói len mục tiêu test
+    @Test
+    public void getFactorial_RunWell_IfVaildArgument(){
+        assertEquals(120, getFactorial(5));
+    }
+    
+    //cần ném về ngoại lệ hay ko
+    //nếu đúng là mày ném về ngoại lệ IllegalArgumentException 
+    //tình huống giao thừa -5 màu xanh   
+    
+    //IllegalArgumentException
+    @Test(expected = SQLException.class)
+    public void getFactorial_ThrowException_IfInvalidArgument(){
+        getFactorial(-5);
+    }
+    //ngoại lệ ko phải là 1 value để đem ra so sánh
+    
+    
+    
+}
