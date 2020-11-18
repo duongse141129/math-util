75f54c5bec92ddf73ff8bef0188fef034a268710
duongse141129 <duong97498@gmail.com>
2020-11-18 08:00:09 +0700

    optimize the getF() by using recursion

--- src/duong/util/MathUtility.java
+++ src/duong/util/MathUtility.java
@@ -21,16 +21,34 @@ public class MathUtility {
     //21! cực lón, tràn kiểu long, chứa ko nổi
     //int thối đa 2 tỷ 1 mấy
     //hàm tính gii thau72 trong giới hạn đầu vào từ 0..20
+//    public static long getFactorial(int n){
+//        if(n<0 || n>20)throw new IllegalArgumentException("n must be between 0..20");
+//        if(n==0 || n==1)
+//            return 1;
+//        //ko sài else có else trừ điểm
+//        long result =1;
+//        for(int i=2;i<=n;i++){
+//            result*=i;
+//        }
+//        return result;
+//    }
+    
+    //viết đệ quy 
+    // gọi lại chính mình vs 1 quy mô khác
+    // đệ quy phải có điểm dừng, ko di miết
+    // rất dễ bị vòng lặp vô tận
+    //kết luận n!=n*(n-1)!
     public static long getFactorial(int n){
-        if(n<0 || n>20)throw new IllegalArgumentException("n must be between 0..20");
-        if(n==0 || n==1)
+        if(n<0 || n >20)throw new IllegalArgumentException("n must between 0...20");
+        if(n ==0 || n==1)
             return 1;
-        //ko sài else có else trừ điểm
-        long result =1;
-        for(int i=2;i<=n;i++){
-            result*=i;
-        }
-        return result;
+        return n*getFactorial(n-1);
     }
+    // mình minh họa khái niệm Refactoring - tối ưu, chỉnh sửa lại code
+    //cho tốt hơn
+    //và minh họa Regression Test, test hồi quy, test lại những gì đã từng test xanh
+    // để check xem code còn Xanh như ngày xưa
+    
+    
 
 }
