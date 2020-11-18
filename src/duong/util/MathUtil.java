47d9c612ac8ddfcb1754a12f4bf069d6597c36b7
duongse141129 <duong97498@gmail.com>
2020-11-18 08:26:20 +0700

    say goodbye message

--- src/duong/util/MathUtil.java
+++ src/duong/util/MathUtil.java
@@ -24,6 +24,9 @@ public class MathUtil {
         
         System.out.println("0!: "+getFactorial(0));
         
+        System.out.println("Goodbye and good luck to you all in the FINAL EXAM");
+        System.out.println("Hope that you all will get the GREEN ICONS");
+        
         System.out.println("-5!: "+getFactorial(-5));
         
         //in thủ PI
